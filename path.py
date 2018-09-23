058058396df52b391893a6320c50ac0694d35948
Harish Chauhan <harish.chauhan99@gmail.com>
2018-09-23 20:44:58 +0530

    optimize methods of path class

--- path.py
+++ path.py
@@ -4,27 +4,25 @@ class Path:
        get the path, grab all the files in that path  
     '''
 
-    def __init__(self, in_path=''):
+    def __init__(self, in_path='.'):
         self.in_path = in_path
         self.file_list = []
-        if self.in_path:
-            self.validate_path()
-        else:
-            self.get_cwd()
+        self.validate_path()
        
     
-    def get_cwd(self):
-        self.in_path = os.getcwd()
-
-    
     def validate_path(self):
-        pass
-
-    def list_dir(self):
-        print(os.listdir(path=self.in_path))
+        if os.path.exists(self.in_path):    #returns True or False
+            if self.in_path == '.' :
+                print ('Searching in current working directory')
+            else:
+                print('searching for files in path %s' %self.in_path)
+        else:
+            print('Invalid path provided, please check the path')
+            sys.exit()
+        
 
     def scan_dir(self):
-        '''it performs better than list_dir '''
+        '''scans all the files in a directory'''
         with os.scandir(self.in_path) as f:
             for entry in f:
                 if not entry.name.startswith('.') and entry.is_file():
@@ -39,12 +37,14 @@ class Path:
 if __name__ == "__main__":
     import os 
     import sys 
+
+    #Check whether user has provided the path or not, if not then search in CWD
     if len(sys.argv) == 2:
         get_path = str(sys.argv[1])
-        test_path = Path(get_path)
-        print(test_path)
-        test_path.scan_dir()
-        print(test_path.file_list)
+        test_path = Path(get_path)          
     else:
-        print("please provide the path name or '.' if CWD")
-        print(sys.argv)
+        test_path = Path() 
+
+    test_path.scan_dir()
+    print(test_path.file_list)
+
